93c737ac45c6a1f8930209f9c13cea5b39b531e6
ana-jaramilloj <123768402+ana-jaramilloj@users.noreply.github.com>
2023-02-10 14:01:06 -0500

    apps1 tranajamos en apps

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 document, addEventListener("DOMContentLoaded", () => {});
+  const cardAdj = [
+  {
+    name: "imagen1.jpg",
+    img: "images/imagen1.jpg.png"
+  },
+
+  ];
+
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscidasId =[];
+  var cartasGanadas
