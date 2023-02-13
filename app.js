bc8f79ea9025432196572aa3046010396d453e83
ana-jaramilloj <123768402+ana-jaramilloj@users.noreply.github.com>
2023-02-13 12:33:32 -0500

    change img

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-document, addEventListener("DOMContentLoaded", () => {});
+//----------------- lecture_01 ----------------------------------//
+document.addEventListener("DOMContentLoaded", () => {
+  //----------------- lecture_02 ----------------------------------//
   const cardAdj = [
-  {
-    name: "imagen1.jpg",
-    img: "images/imagen1.jpg.png"
-  },
-
+    { name: "c", img: "images/imagen1.jpg" },
+    { name: "cpp", img: "images/img 2.jpg" },
+    { name: "css", img: "images/img 3.jpg" },
+    { name: "fortran", img: "images/img 4.jpg" },
+    { name: "html", img: "images/img 5.jpg" },
+    { name: "python", img: "images/img 6.jpg" },
+    { name: "c", img: "images/img 7.jpg" },
+    { name: "csharp", img: "images/img 8.jpg" }
   ];
 
   const cuadricula = document.querySelector(".cuadricula");
   const resultado = document.querySelector("#resultado");
   var cartasEscogidas = [];
-  var cartasEscidasId =[];
-  var cartasGanadas
+  var cartasEscogidasId = [];
+  var cartasGanadas = [];
+
+  //----------------- lecture_03 ----------------------------------//
+  function crearTablero() {
+    for (let i = 0; i < cardAdj.length; i++) {
+      // Loop que inicia en i=0, se incrementa de 1 en 1 (i++) y finaliza
+      // cuando i=cardAdj.length, es decir, 12.
+      var carta = document.createElement("img"); // Crea elemento img cada vez que se ejecuta la función.
+      carta.setAttribute("src", "images/reverso.png"); //Asignar nuevo atributo a cada carta
+      //igual a la imagen 'reverso'.
+      carta.setAttribute("data-id", i); //Asignar como atributo a cada carta creada, el id=i.
+      carta.addEventListener("click", voltearCarta); //Detectar si la carta asignada recibe un click y ejecuta
+      //la función voltearCarta.
+      cuadricula.appendChild(carta); // Relaciona la carta creada con la variable cuadricula enlazada con la clase
+      // cuadricula de html para que se imprima la carta creada en el div de html.
+    }
+  }
+});
