f0f4320911a29b56a46bebd9381d8fc4b02a3be0
ana-jaramilloj <123768402+ana-jaramilloj@users.noreply.github.com>
2023-03-13 12:29:51 -0500

    yde

--- app.js
+++ app.js
@@ -2,14 +2,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   //----------------- lecture_02 ----------------------------------//
   const cardAdj = [
-    { name: "c", img: "images/imagen1.jpg" },
+    { name: "imagen1", img: "images/imagen1.jpg" },
     { name: "cpp", img: "images/img 2.jpg" },
     { name: "css", img: "images/img 3.jpg" },
     { name: "fortran", img: "images/img 4.jpg" },
     { name: "html", img: "images/img 5.jpg" },
     { name: "python", img: "images/img 6.jpg" },
-    { name: "c", img: "images/img 7.jpg" },
-    { name: "csharp", img: "images/img 8.jpg" }
+    { name: "imagen1", img: "images/imagen1.jpg" },
+    { name: "cpp", img: "images/img 2.jpg" },
+    { name: "css", img: "images/img 3.jpg" },
+    { name: "fortran", img: "images/img 4.jpg" },
+    { name: "html", img: "images/img 5.jpg" },
+    { name: "python", img: "images/img 6.jpg" }
   ];
 
   const cuadricula = document.querySelector(".cuadricula");
@@ -21,28 +25,26 @@ document.addEventListener("DOMContentLoaded", () => {
   //----------------- lecture_03 ----------------------------------//
   function crearTablero() {
     for (let i = 0; i < cardAdj.length; i++) {
-      
-      var carta = document.createElement("img"); 
-      carta.setAttribute("src", "images/reverso.png"); 
-      
-      carta.setAttribute("data-id", i); 
+      var carta = document.createElement("img");
+      carta.setAttribute("src", "images/reverso.png");
 
-      carta.addEventListener("click", voltearCarta); 
-  
-      cuadricula.appendChild(carta); 
-      
+      carta.setAttribute("data-id", i);
+
+      carta.addEventListener("click", voltearCarta);
+
+      cuadricula.appendChild(carta);
     }
   }
 
-//________________ lecture_03 __________________________________________//
-function voltearCarta(){
-  var cardId = this.getAttribute("data-id");
-  cartasEscogidas.push(cardAdj[cardId].name);
-  cartasEscogidasId.push(cardId);
-  this.setAttribute("src", cardAdj[cardId].img);
-  if (cartasEscogidas.length === 2) { 
-    setTimeout(verificarPareja, 1000);
+  //________________ lecture_03 __________________________________________//
+  function voltearCarta() {
+    var cardId = this.getAttribute("data-id");
+    cartasEscogidas.push(cardAdj[cardId].name);
+    cartasEscogidasId.push(cardId);
+    this.setAttribute("src", cardAdj[cardId].img);
+    if (cartasEscogidas.length === 2) {
+      setTimeout(verificarPareja, 1000);
+    }
   }
-} 
   crearTablero();
 });
